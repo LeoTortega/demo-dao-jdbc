48c2784f8e1c5c871fdc2d06f0bfc231a12c800c
Leonardo Torres Ortega <leonardo.tortega@outlook.com>
2023-03-06 10:11:05 -0300

    SellerDaoJDBC DaoFactory

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -8,5 +10,8 @@ public class Main {
         Department obj = new Department(1, "Books");
         Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
         System.out.println(seller);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
     }
 }
